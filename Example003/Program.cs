2758240c6dcd94828d7f87928680654060a766cb
OLGA <1986o2019@mail.ru>
2023-07-08 23:57:58 +0300

    Example003

---
+++ Example003/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+    Console.WriteLine(a);
+else
+    Console.WriteLine(b);
